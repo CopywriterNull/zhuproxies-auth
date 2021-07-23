802dc175b6af6c7bfa8dbd6253e16717f4880a05
CopywriterNull <46082866+CopywriterNull@users.noreply.github.com>
2021-07-22 22:43:46 -0700

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,19 @@
+from flask import Flash,request, render_template,redirect,session
+from oauth import Oauth
+
+app = Flash(__name__)
+@app.route("/",methods = ["get"])
+def index():
+  return redirect()
+  
+  
+@app.route("/login",methods = ["get"])
+def login();
+  return "logged in"
+
+if(__name__ == "__main__"):
+  app.run(debug=True)
+
+
+class object(oAuth)
+{ 
